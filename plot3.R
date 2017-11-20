aa3fa0e93fcb6c5734136e050e05c622ada9bc1c
judand <javierf.durand@gmail.com>
2017-11-20 17:39:28 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,30 +1,13 @@
 
 
-> getwd()
-[1] "d:/Documents"
-> data<-read.table("household.txt",header = TRUE,sep = ";", stringsAsFactors = FALSE)
-> #converting the "Date"  with as.date()
-> data$Date<-as.Date(data$Date,"%d/%m/%Y")
-> #Subsetting the data with the necessary dates
-> data3ex<-subset(data,Date >= "2007-02-01" & Date <= "2007-02-02")
-> #converting the type of "Time" with as.POSIXct()
-> data3ex$Time<-as.POSIXct(paste(data3ex$Date, data3ex$Time), format="%Y-%m-%d %H:%M:%S")
-> #converting the rest of the columns into numeric with as.numeric()
-> data3ex[,3:9]<-sapply(data3ex[,3:9], as.numeric)
-> #Creating the Time series object model for necessary features
-> submetering1series<-ts(data3ex$Sub_metering_1,frequency = 1440,start = 0)
-> submetering2series<-ts(data3ex$Sub_metering_2,frequency = 1440,start = 0)
-> submetering3series<-ts(data3ex$Sub_metering_3,frequency = 1440,start = 0)
-> #Creating the file 
-> png(filename = "plot3.png",width=480, height=480, units= "px")
-> plot.ts(submetering1series,ylab = "Energy sub metering",ylim = c(0,40),xlab = "Days")
-> par(new= T)
-> plot.ts(submetering2series,ylab = "Energy sub metering",xlab = "Days",ylim = c(0,40),col = "red")
-> par(new= T)
-> plot.ts(submetering3series,ylab = "Energy sub metering",xlab = "Days",ylim = c(0,40),col = "blue")
-> legend("topright",legend = c("sub_metering_1","sub_metering_2","sub_metering_3"),lwd = c(1,1,1),col = c("black","red","blue"))
-> dev.off()
-null device 
-          1 
+file <- read.table("/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+usedData <- file[file$Date %in% c("1/2/2007","2/2/2007"),]
+setTime <- strptime(paste(usedData$Date, usedData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalFile <- cbind(setTime,usedData)
 
-> 
+png(file="plot3.png", bg="white",width=480, height=480, units= "px")
+plot(finalFile$setTime, finalFile$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(finalFile$setTime, finalFile$Sub_metering_2, col="red")
+lines(finalFile$setTime, finalFile$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_1=2","Sub_metering_3"), col=c("black","red","blue"),lty="solid")
+dev.off()
