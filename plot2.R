5f16267a898d3b81ee3661d0954f4ac1c67a7f0e
judand <javierf.durand@gmail.com>
2017-11-21 20:27:36 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
-
-
->file <- read.table("/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+#code of plot2
+file <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
 usedData <- file[file$Date %in% c("1/2/2007","2/2/2007"),]
 setTime <- strptime(paste(usedData$Date, usedData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalFile <- cbind(setTime,usedData)
