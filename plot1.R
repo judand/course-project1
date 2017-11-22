2216acf79824301b31e109c703f542e6a4b7fca8
judand <javierf.durand@gmail.com>
2017-11-22 13:02:16 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,8 @@
-file <- read.table("/household_power_consumption.txt", header=T, sep=";", na.strings="?")
-usedData <- file[file$Date %in% c("1/2/2007","2/2/2007"),]
-setTime <- strptime(paste(usedData$Date, usedData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
-finalFile <- cbind(setTime,usedData)
+arch <- read.table("/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+dataused <- arch[arch$Date %in% c("1/2/2007","2/2/2007"),]
+setTime <- strptime(paste(dataused$Date, dataused$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalFile <- cbind(setTime,dataused)
 
-png(file="plot1.png",bg="white",width=480, height=480, units= "px")
+png(arch="plot1.png",bg="white",width=480, height=480, units= "px")
 hist(finalFile$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frequency")
-dev.off()
-
+dev.off() 
